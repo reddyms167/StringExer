db8dd249bd16b69c0c3ca08ce4f1efa1a4a38ac8
reddyms167 <reddyms167@gmail.com>
2015-02-16 17:34:15 -0600

    Added a new class VowelsInString which calculates vowels and strings in a given string and prints them out.

--- StringExer/src/org/manish/stringexer/VowelsInString.java
+++ StringExer/src/org/manish/stringexer/VowelsInString.java
@@ -1,5 +1,8 @@
 package org.manish.stringexer;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
 
 public class VowelsInString {
@@ -7,25 +10,44 @@ public class VowelsInString {
 	public static void main(String[] args) {
 		System.out.println("Enter some text:");
 		Scanner in = new Scanner(System.in);
-		countVowels(in.nextLine());
+		PrintAllVowelsAndConsonants(in.nextLine());
 		in.close();
 
 	}
 
-	public static void countVowels(String word) {
+	public static void PrintAllVowelsAndConsonants(String word) {
 		int vowels = 0;
 		int consonants = 0;
 
-		for (char ch : word.toCharArray()) {
+		// These are the ArrayList which store the vowels and consonants
+		// respectively...
+		List<Character> vowelList = new ArrayList<Character>();
+		List<Character> consonantList = new ArrayList<Character>();
+
+		// This loops through each character in the given string...
+
+		for (int i = 0; i < word.length(); i++) {
+			char ch = word.charAt(i);
 			if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+				vowelList.add(ch);
 				vowels++;
 			} else {
+				consonantList.add(ch);
 				consonants++;
 			}
-
 		}
-		System.out.println("Vowels in the String [" + word + "] " + vowels
-				+ " and the consonants: " + consonants);
+
+		System.out.println("Vowels in String [" + word + "] : " + vowels
+				+ " and consonants are " + consonants);
+
+		System.out
+				.println("-----------------------------------------------------");
+
+		System.out.println("Vowels in the String [" + word + "] are: "
+				+ vowelList);
+		System.out.println("Consonants in the String [" + word + "] are: "
+				+ consonantList);
 
 	}
+
 }
